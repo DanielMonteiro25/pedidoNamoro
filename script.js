0eaf4a9c7628906e1537f1ae47a1cedbd54eec01
Daniel <123267716+DanielMonteiro25@users.noreply.github.com>
2024-07-08 23:22:43 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+var moveButton = document.querySelector("#moveButton");
+var correctlyButton = document.querySelector("#correctlyButton");
+
+moveButton.addEventListener("click", function () {
+    var vertical = Math.floor(Math.random() * 400);
+    var horizontal = Math.floor(Math.random() * 400);
+    moveButton.style.position = "absolute";
+    moveButton.style.left = vertical + "px";
+    moveButton.style.top = horizontal + "px";
+});
+
+correctlyButton.addEventListener("click", function () {
+    alert("Resposta correta! Vamo comer um empadão");
+});
